afe4522f5c2e5d2c34231e185d0ab93077b66015
Atul Kumar Mishra <atulkm189@gmail.com>
2025-07-27 13:49:22 +0530

    init

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '10mb' }));
 
 // Enhanced CORS configuration
 app.use(cors({
-    origin: ['https://atul-k-m.github.io/ecell-feedback/', 'http://127.0.0.1:5500', 'http://localhost:3000'],
+    origin: ['https://atul-k-m.github.io/ecell-feedback', 'http://127.0.0.1:5500', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
